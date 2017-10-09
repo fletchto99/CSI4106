7744c4c2882c7567fd9f7a4b1aa5eda750a117cd
Billard94 <alexbillard94@gmail.com>
2017-10-09 14:35:21 -0400

    DFS implemented

--- assignments/assignment 1/searchdir/blindSearch/depthfirst_search.py
+++ assignments/assignment 1/searchdir/blindSearch/depthfirst_search.py
@@ -5,10 +5,17 @@ from searchdir.util import *
 ## It must return the solution node and the number of visited nodes
 def depthfirst_search(initialState):
 
+	v = Node(initialState)
+	# visited = {}
+	# s = stack()
     visited, s = stack(), [initialState]
-    while s:
-         vertex = s.pop()
-        if vertex not in visited:
-             visited.push(vertex)
-             s.push(graph[vertex] - visited)
-     return visited
+    while not s.isEmpty():
+    	v = s.pop()
+    	if v.state.isGoal():
+    		return v, len(visited)
+    	else:
+            for nextState in v.state.possibleActions(): # for actions
+                newVertex = v._createNode(nextState)      # create new vertex from children
+                if newVertex.elem not in visited :    # if new node is not visited
+                    s.push(newVertex)             # push the newnode
+                    visited[newVertex.elem] = newVertex.getcost()       # list up the visited with cost
