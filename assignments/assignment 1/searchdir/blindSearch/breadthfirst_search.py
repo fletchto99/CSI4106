f47c9eaac711e6d8779d8ca538042c41d2ea74ca
Billard94 <alexbillard94@gmail.com>
2017-10-09 17:53:50 -0400

    DFS and BFS

--- assignments/assignment 1/searchdir/blindSearch/breadthfirst_search.py
+++ assignments/assignment 1/searchdir/blindSearch/breadthfirst_search.py
@@ -6,19 +6,19 @@ from searchdir.util import *
 def breadthfirst_search(initialState):
     print('BFS------------------------------')
     first = Node(initialState)
-    visited = {}
+    visited = []
     q = Queue()
     q.enqueue(first)
-    visited[first.elem] = first.getcost()  # Retreive the cost of the first state
 
     while not q.isEmpty():
         v = q.dequeue()  # dequeue the vertex (or node)
+        visited.append(v.getcost())
         if not v.state.isGoal():
             for _next in v.state.possibleActions(): # Find all next possible actions
                 newV = v._createNode(_next)      # create a new vertex (or node)
-                if newV.elem not in visited :    # if new vertex has not been explored
+                if newV not in visited :    # if new vertex has not been explored
                     q.enqueue(newV)  # enqueue the new vertice
-                    visited[newV.elem] = newV.getcost()   # add the cost to the list
+                    visited.append(newV.getcost())  # add the cost to the list
 
         else:
         	return v, len(visited) # return solution
--- assignments/assignment 1/searchdir/blindSearch/depthfirst_search.py
+++ assignments/assignment 1/searchdir/blindSearch/depthfirst_search.py
@@ -4,20 +4,21 @@ from searchdir.util import *
 ## This method must implement depdth-first search (DFS)
 ## It must return the solution node and the number of visited nodes
 def depthfirst_search(initialState):
+    print('DFS------------------------------')
     first = Node(initialState)
-    visited = {}
-    s = stack()
+    visited = []
+    s = Stack()
     s.push(first)
-    visited[first.elem] = first.getcost()  # Retreive the cost of the first state
+    visited.append(0)
 
     while not s.isEmpty():
         v = s.pop()
+        visited.append(v.getcost())
         if not v.state.isGoal():
-            for _next in v.state.possibleActions():
-                newV = v._createNode(_next)      # create new vertex from children
-                if newV.elem not in visited :    # if new node is not visited
-                    s.push(newV)             # push the newnode
-                    visited[newV.elem] = newV.getcost()       # list up the visited with cost
+            for _next in v.expand():
+                if _next.state not in visited:
+                    s.push(_next)
+                    visited.append(v.getcost())
         else:
             return v, len(visited)
 
