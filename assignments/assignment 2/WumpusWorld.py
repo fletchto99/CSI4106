a875ba2ad3cbec5c17f1dbf333daf0ecfa2146ae
Billard94 <alexbillard94@gmail.com>
2017-11-20 19:48:45 -0500

    The game is only a 4x4 matrix, nothing else

--- assignments/assignment 2/WumpusWorld.py
+++ assignments/assignment 2/WumpusWorld.py
@@ -2,28 +2,28 @@ import random
 
 
 class WumpusWorld:
-    def __init__(self, width, height):
+    def __init__(self):
 
         self.agent = {
             "x": 0,
             "y": 0
         }
 
-        self.gold = self.__randspot(width, height)
+        self.gold = self.__randspot()
 
         while self.gold['x'] == 0 and self.gold['y'] == 0:
-            self.gold = self.__randspot(width, height)
+            self.gold = self.__randspot()
 
-        self.wumpus = self.__randspot(width, height)
+        self.wumpus = self.__randspot()
 
         while (self.wumpus['x'] == 0 and self.wumpus['y'] == 0) or (
                 self.wumpus['x'] == self.gold['y'] and self.wumpus['y'] == self.gold['y']):
-            self.gold = self.__randspot(width, height)
+            self.gold = self.__randspot()
 
-        self.board = [[set() for _ in range(width)] for _ in range(height)]
+        self.board = [[set() for _ in range(4)] for _ in range(4)]
 
-        for row in range(0, height):
-            for col in range(0, width):
+        for row in range(0, 4):
+            for col in range(0, 4):
                 pit = True
 
                 if col == self.agent['x'] and row == self.agent['y']:
@@ -47,29 +47,22 @@ class WumpusWorld:
                     self.board[row][col].add("P")
                     if col > 0:
                         self.board[row][col - 1].add("B")
-                    if col < width - 1:
+                    if col < 4 - 1:
                         self.board[row][col + 1].add("B")
                     if row > 0:
                         self.board[row - 1][col].add("B")
-                    if row < height - 1:
+                    if row < 4 - 1:
                         self.board[row + 1][col].add("B")
 
-    def __randspot(self, width, height):
+    def __randspot(self):
         return {
-            "x": random.randint(0, width - 1),
-            "y": random.randint(0, height - 1)
+            "x": random.randint(0, 4 - 1),
+            "y": random.randint(0, 4 - 1)
         }
 
     def printBoard(self):
-        height = len(self.board)-1
         count = 0
-        width = len(self.board[0])-1
-        wall = ""
-        room = ""
-        for i in range(len(self.board[0])):
-            wall = wall + "==========="
-            room = room + "-----------"
-        print(wall)
+        print("============================================")
         for row in self.board:
             print("|| ", end='')
             for col in row:
@@ -84,10 +77,10 @@ class WumpusWorld:
                 else:
                     print(" " + str(list(col).__str__().replace('{','').replace('}','').replace('[','').replace(']','').replace(',','').replace("'",'')) + " |", end='')
 
-            if (count == height):
-                print("|\n" + wall)
+            if (count == 3):
+                print("|\n============================================")
             else:
-                print("|\n" + room)
+                print("|\n--------------------------------------------")
             count = count + 1
         print("\n\n\n")
         for row in self.board:
@@ -99,5 +92,5 @@ class WumpusWorld:
 
 
 # Test Case
-a = WumpusWorld(4, 4)
+a = WumpusWorld()
 a.printBoard()
