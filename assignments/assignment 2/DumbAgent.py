0de8c11aff8c3b17a8ac48a65bf55a8b5a4b8357
Matt Langlois <fletchto99@gmail.com>
2017-11-21 21:55:36 -0500

    Some more comments in the dumb agent

--- assignments/assignment 2/DumbAgent.py
+++ assignments/assignment 2/DumbAgent.py
@@ -2,67 +2,91 @@ import random
 import time
 
 
+# The dumb agent based off of the criteria from the assignment
 class DumbAgent:
+
     def __init__(self, WumpusWorld, debug):
         self.world = WumpusWorld
         self.fired = False
         self.actions = []
         self.debug = debug
 
+    # Navigates through the world, returning some statistics about its journey
     def navigate(self):
+
+        # Print out the starting world if debug mode is enabled
         if self.debug:
             print('Starting world')
             self.world.printBoard()
             print("\n\n\n")
 
+        # Compute the start time
         start = int(round(time.time() * 1000))
+
+        # Loop until we either die or find the gold
         while True:
+            # local vars for tracking
             justFired = False
             result = ''
+
+            # Check if we found the gold, if so apply the pickup action
             if 'G' in self.world.getLocationProperties():
-                self.actions.append('pickup')
+                # Attempt to pickup the gold
                 if self.world.pickup():
                     self.actions.append('pickup')
+            # Attempt to fire the bow if we detect a stench and we didn't just fire the bow
             elif not self.fired and 'S' in self.world.getLocationProperties():
-                self.actions.append('fire')
                 self.world.fireArrow()
+                self.actions.append('fire')
+
+                # Track if we fired the arrow
                 self.fired = True
                 justFired = True
             else:
+                # Randomly apply one of the three remaining actions
                 rand = random.randint(1, 3)
                 if rand == 1:
-                    self.actions.append('left')
                     self.world.turnLeft()
+                    self.actions.append('left')
                 elif rand == 2:
-                    self.actions.append('right')
                     self.world.turnRight()
+                    self.actions.append('right')
                 elif rand == 3:
-                    self.actions.append('step')
+                    # The result of stepping is tracked, in case we die or walk into a wall
                     result = self.world.step()
+                    self.actions.append('step')
 
-            if not justFired:
+            # Checks if the user didn't just fire their arrow and if so reset the fired status
+            if not justFired and self.fired:
                 self.fired = False
 
             if self.debug:
+                # prints out some details about the current run
                 print("Action Performed: " + str(self.actions[-1]))
+                if str(self.actions[-1]) and result == 'bump':
+                    print('You bumped into the wall')
                 print("World now:")
                 self.world.printBoard()
                 print("\n\n\n")
 
+            # Checks if the user has died, if so stop looping
             if result == 'dead':
                 self.actions.append('dead')
                 if self.debug:
                     print('You Died!')
                 break
 
+            # Checks if the user has found the gold, if so stop looping
             if self.actions[-1] == 'pickup':
                 if self.debug:
                     print('The gold has been found!')
                 break
 
+        # Tidy the output for the next map
         if self.debug:
-            print("\n\n\n")
+            print("\n\n")
 
+        # Compute the score of the journey
         score = 0
         for action in self.actions:
             if action == 'pickup':
@@ -73,8 +97,11 @@ class DumbAgent:
                 score = score - 10
             else:
                 score = score - 1
+
+        # Return some statistics which will be required for the report
         return {
             "score": score,
+            # The depth is -1 because the last action is not an action but rather the result
             "depth": len(self.actions)-1,
             "time": int(round(time.time() * 1000)) - start,
             "optimal": False,
--- assignments/assignment 2/Main.py
+++ assignments/assignment 2/Main.py
@@ -2,21 +2,23 @@ from WumpusWorld import WumpusWorld
 from DumbAgent import DumbAgent
 from IntellientAgent import IntelligentAgent
 
-# Used for statistics
+print('Running...\n\n')
+
+# The entrypoint for the application
 iterations = 10000
+
+# Used for statistics
 totalScore = 0
 totalDepth = 0
 totalTime = 0
 
-print('Dumb Agent\n\n')
-
 for i in range(iterations):
     # Generate a world, where the 0th world is the one from the assignment
-    world = WumpusWorld(iterations)
+    world = WumpusWorld(i)
 
     # Always ensure the world is solvable
     while not world.solvable():
-        world = WumpusWorld(iterations)
+        world = WumpusWorld(i)
 
     # Agent without debugging (if debugging is enabled it will print out the map)
     agent = DumbAgent(world, False)
@@ -29,6 +31,7 @@ for i in range(iterations):
     totalDepth = totalDepth + result['depth']
     totalTime = totalTime + result['time']
 
+print('Dumb Agent:')
 print('Total execution time in seconds is: ' + str(totalTime/1000))
 print('Final avg score is: ' + str(totalScore/iterations))
 print('Final avg depth is: ' + str(totalDepth/iterations))
@@ -36,20 +39,17 @@ print('Final avg time in ms is: ' + str(totalTime/iterations))
 
 
 # Used for statistics
-iterations = 10000
 totalScore = 0
 totalDepth = 0
 totalTime = 0
 
-print('Intelligent Agent\n\n')
-
 for i in range(iterations):
     # Generate a world, where the 0th world is the one from the assignment
-    world = WumpusWorld(iterations)
+    world = WumpusWorld(i)
 
     # Always ensure the world is solvable
     while not world.solvable():
-        world = WumpusWorld(iterations)
+        world = WumpusWorld(i)
 
     # Agent without debugging (if debugging is enabled it will print out the map)
     agent = IntelligentAgent(world, True)
@@ -62,6 +62,7 @@ for i in range(iterations):
     totalDepth = totalDepth + result['depth']
     totalTime = totalTime + result['time']
 
+print('\n\nIntelligent Agent:')
 print('Total execution time in seconds is: ' + str(totalTime/1000))
 print('Final avg score is: ' + str(totalScore/iterations))
 print('Final avg depth is: ' + str(totalDepth/iterations))
