dc6c42a5f1604cad57d5866199196cf26d575f11
Matt Langlois <fletchto99@gmail.com>
2017-11-22 23:02:32 -0500

    Incomplete intelligent agent

--- assignments/assignment 2/IntellientAgent.py
+++ assignments/assignment 2/IntellientAgent.py
@@ -1,6 +1,7 @@
 import time
 
 from aima.logic import *
+from PriorityQueue import *
 
 
 # The intelligent agent based off of propositional logic
@@ -11,8 +12,7 @@ class IntelligentAgent:
         self.actions = []
         self.debug = debug
         self.kb = PropKB()
-        self.locations = []
-        self.board = [[set() for _ in range(4)] for _ in range(4)]
+        self.board = [[[False, ''] for _ in range(4)] for _ in range(4)]
 
     # Navigates through the world, returning some statistics about its journey
     def navigate(self):
@@ -30,9 +30,7 @@ class IntelligentAgent:
         while True:
             # local vars for tracking
             result = ''
-
-            # Build a visited list
-            self.locations.append(self.world.getAgentLocation())
+            location = self.world.getAgentLocation()
 
             # Check if we found the gold, if so apply the pickup action
             if 'G' in self.world.getLocationProperties():
@@ -43,39 +41,44 @@ class IntelligentAgent:
             # Find the percepts for the current location
             perceptions = self.world.getLocationProperties()
 
-            tell('P{}{}').format(x,y)
+            grid = self.board[location['y']][location['x']]
 
             # Add the percepts to a knowledge base using AIMA open source libraries (as recommended in the assignment)
-            for perception in perceptions:
-                self.kb.tell({
-                    "x": self.world.getAgentLocation()['x'],
-                    "y": self.world.getAgentLocation()['y'],
-                    "perception": str(perception)
-                })
+            # Only add the percepts if we haven't visited this grid location yet
+            if grid[1] == '':
+                for perception in perceptions:
+                    self.kb.tell('{}{}{}'.format(str(perception), location['x'], location['y']))
 
-            agent = self.world.getAgentLocation()
-            self.board[agent["y"]][agent["x"]] = self.world.getLocationProperties()
-            for perception in perceptions:
-                self.kb.ask({
+                res = 'u' if self.kb.ask('B{}{}^S{}{}'.format(location['x'], location['y'],location['x'], location['y'])) else 's'
 
-                    })
+                # Mark the current grid location as visited and tell the user if it + the squares around it are safe
+                self.board[location['y']][location['x']][0] = True
+                self.board[location['y']][location['x']][1] = res
+
+                if res == 's':
+                    if location['x'] > 0 and self.board[location['y']][location['x'] - 1][1] == '':
+                        self.board[location['y']][location['x'] - 1][1] = 's'
+                    if location['x'] < 3 and self.board[location['y']][location['x'] + 1][1] == '':
+                        self.board[location['y']][location['x'] + 1][1] = 's'
+                    if location['y'] > 0 and self.board[location['y'] - 1][location['x']][1] == '':
+                        self.board[location['y'] - 1][location['x'] - 1][1] = 's'
+                    if location['y'] < 3 and self.board[location['y'] + 1][location['x']][1] == '':
+                        self.board[location['y'] + 1][location['x']][1] = 's'
 
 
-            # Look for rooms which lead to potentially unsafe rooms
-            unsafePath = self.kb.ask({
-                "perception": "S^B"
-            })
+
+
 
             # Check if we found the gold, if so apply the pickup action
-            if 'G' in self.world.getLocationProperties():
+            if 'G' in perceptions:
                 # Attempt to pickup the gold
                 if self.world.pickup():
                     self.actions.append('pickup')
+
             # Attempt to fire the bow if we detect a stench and we didn't just fire the bow
             else:
-                loc = self.world.getAgentLocation()
 
-                if loc['x'] == unsafePath['x'] and loc['y'] == unsafePath['y']:
+                if location['x'] == unsafePath['x'] and location['y'] == unsafePath['y']:
                     # Turn around if we're heading on an unsafe path to bo back to a known safe ares
                     # the goal is to map out all safe areas before exploring unknone areas (i.e. find
                     # the wumpus an kill it)
@@ -132,3 +135,44 @@ class IntelligentAgent:
             "optimal": False,
             "complete": False
         }
+
+    # Manhattan distance from one position to another
+    def __heuristic(self, coord1, coord2):
+        return abs(coord1[0] - coord2[0]) + abs(coord1[1] - coord2[1])
+
+    # Expand the frontier to only nodes which don't contain pits. All other tiles are valid to traverse to
+    def __expand(self, coord):
+        values = []
+
+        if coord[0] > 0 and 'P' not in self.board[coord[1]][coord[0] - 1]:
+            values.append((coord[0] - 1, coord[1]))
+        if coord[0] < 3 and 'P' not in self.board[coord[1]][coord[0] + 1]:
+            values.append((coord[0] + 1, coord[1]))
+        if coord[1] > 0 and 'P' not in self.board[coord[1] - 1][coord[0]]:
+            values.append((coord[0], coord[1] - 1))
+        if coord[1] < 3 and 'P' not in self.board[coord[1] + 1][coord[0]]:
+            values.append((coord[0], coord[1] + 1))
+        return values
+
+    def astart_closest(self, startState, endState):
+
+        pq_open = PriorityQueue((lambda x, y: y[0] - x[0]))
+        pq_open.enqueue((self.__heuristic(startState, endState), startState, None))
+        closed = set()
+
+        while not pq_open.isEmpty():
+            current_node = pq_open.dequeue()
+            if current_node[1] == endState:
+                path = []
+                while current_node is not None:
+                    path.append(current_node[2])
+                    current_node = current_node[2]
+                path.reverse()
+                return path
+
+            closed.add(current_node[1])
+            for child in self.__expand(current_node[1]):
+                if child not in closed:
+                    pq_open.enqueue((self.__heuristic(startState, endState), child, current_node))
+
+        return []
